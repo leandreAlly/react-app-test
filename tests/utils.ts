35970250d13b40b392a44797513945adb7f439ad
leandre <tuyambazeleandre@gmail.com>
2024-06-27 06:50:41 +0200

    test authStatus component

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,3 +1,4 @@
+import { useAuth0, User } from '@auth0/auth0-react';
 import { delay, http, HttpResponse } from 'msw';
 import { server } from './mocks/server';
 
@@ -12,4 +13,23 @@ export const simulateDelay = (endpoint: string) => {
 
 export const simulateError = (endpoint: string) => {
   server.use(http.get(endpoint, () => HttpResponse.error()));
+};
+
+type Authstate = {
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  user: User | undefined;
+};
+
+export const mockAuthState = (authState: Authstate) => {
+  vi.mocked(useAuth0).mockReturnValue({
+    ...authState,
+    getAccessTokenSilently: vi.fn().mockResolvedValue('a'),
+    getAccessTokenWithPopup: vi.fn(),
+    getIdTokenClaims: vi.fn(),
+    loginWithRedirect: vi.fn(),
+    loginWithPopup: vi.fn(),
+    logout: vi.fn(),
+    handleRedirectCallback: vi.fn(),
+  });
 };
